2170b1e91dd7d595a9a94b33e48e7560de9894c5
David Shaw <david.shaw1242@gmail.com>
2025-02-22 11:50:50 -0500

    setup of the footer with some links to my informatino and mobile friendly layout

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,35 @@
 import "./Footer.css"
 
-export default function Footer() {
+interface FooterProps {
+    footerVals: {
+        link_title: string,
+        link_path: string,
+        svg: React.ReactNode;
+    }[];
+}
+
+export default function Footer({ footerVals }: FooterProps) {
+    const d = new Date();
+    let year = d.getFullYear();
+
     return (
         <div className="footer">
-            
+            <p className="info">My Info: </p>
+            <ul className="info-list link-underline">
+                {footerVals.map((val, index) =>
+                    <li className="info-list-item" key={index}>
+                        <a href={val.link_path}>
+                            <div className="info-list-item-svg">
+                                {val.svg}
+                            </div>
+                            <div className="info-list-item-title">
+                                {val.link_title}
+                            </div>
+                        </a>
+                    </li>
+                )}
+            </ul>
+            <p className="year"> &#169; {year}</p>
         </div>
     )
 }
