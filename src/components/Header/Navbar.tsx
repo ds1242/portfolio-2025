4bad42e7388dddb97e87237a8e8403f77de246b7
David Shaw <david.shaw1242@gmail.com>
2025-02-22 09:59:01 -0500

    made the navbar slide in and out

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './Navbar.css';
 
 interface HeaderProps {
@@ -9,12 +10,13 @@ interface HeaderProps {
 }
 
 export default function Navbar({ headerVals }:HeaderProps) {
+    const [isActive, setIsActive] = useState(false);
 
     const vals: HeaderProps[] = headerVals;
 
     const toggleMenu = () => {
-        const menu = document.querySelector('.menu');
-        menu.classList.toggle('active');
+        console.log("clicked")
+        setIsActive(!isActive);
     }
 
     console.log(vals)
@@ -23,7 +25,8 @@ export default function Navbar({ headerVals }:HeaderProps) {
             <h1 className='brand'>
                 David Shaw
             </h1>
-            <ul className='menu links'>
+            <ul className={`menu links ${isActive ? 'active' : ''}`}>
+            {/* <ul className='menu links'> */}
                 {vals.map(val =>
                     // {console.log(val)}
                     <li className='link-item' key={val.header_id + val.link_title}>
@@ -35,10 +38,9 @@ export default function Navbar({ headerVals }:HeaderProps) {
                 )}
             </ul>
             <div className='hamburger' onClick={toggleMenu}>
-
-            <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path d="M3 17h18a1 1 0 0 1 .117 1.993L21 19H3a1 1 0 0 1-.117-1.993L3 17h18H3Zm0-6 18-.002a1 1 0 0 1 .117 1.993l-.117.007L3 13a1 1 0 0 1-.117-1.993L3 11l18-.002L3 11Zm0-6h18a1 1 0 0 1 .117 1.993L21 7H3a1 1 0 0 1-.117-1.993L3 5h18H3Z" fill="#f1faee"/>
-            </svg>
+                <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M3 17h18a1 1 0 0 1 .117 1.993L21 19H3a1 1 0 0 1-.117-1.993L3 17h18H3Zm0-6 18-.002a1 1 0 0 1 .117 1.993l-.117.007L3 13a1 1 0 0 1-.117-1.993L3 11l18-.002L3 11Zm0-6h18a1 1 0 0 1 .117 1.993L21 7H3a1 1 0 0 1-.117-1.993L3 5h18H3Z" fill="#f1faee"/>
+                </svg>
             </div>
         </div>
     )
