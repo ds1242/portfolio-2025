e35ea7460b9e49ba094c941135f1d6bca3d4b380
David Shaw <david.shaw1242@gmail.com>
2025-02-22 09:02:46 -0500

    created responsive hamburger menu

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -12,13 +12,18 @@ export default function Navbar({ headerVals }:HeaderProps) {
 
     const vals: HeaderProps[] = headerVals;
 
+    const toggleMenu = () => {
+        const menu = document.querySelector('.menu');
+        menu.classList.toggle('active');
+    }
+
     console.log(vals)
     return (
         <div className="dark-background-text light-background-text navbar">
             <h1 className='brand'>
                 David Shaw
             </h1>
-            <ul className='links'>
+            <ul className='menu links'>
                 {vals.map(val =>
                     // {console.log(val)}
                     <li className='link-item' key={val.header_id + val.link_title}>
@@ -28,8 +33,13 @@ export default function Navbar({ headerVals }:HeaderProps) {
                         </a>
                     </li>
                 )}
-
             </ul>
+            <div className='hamburger' onClick={toggleMenu}>
+
+            <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3 17h18a1 1 0 0 1 .117 1.993L21 19H3a1 1 0 0 1-.117-1.993L3 17h18H3Zm0-6 18-.002a1 1 0 0 1 .117 1.993l-.117.007L3 13a1 1 0 0 1-.117-1.993L3 11l18-.002L3 11Zm0-6h18a1 1 0 0 1 .117 1.993L21 7H3a1 1 0 0 1-.117-1.993L3 5h18H3Z" fill="#f1faee"/>
+            </svg>
+            </div>
         </div>
     )
 }
