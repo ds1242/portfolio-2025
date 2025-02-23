08f6f7e19cccbcd06152807baca8a567c27be805
David Shaw <david.shaw1242@gmail.com>
2025-02-23 13:23:36 -0500

    more progress setting up content page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -9,7 +9,10 @@ export default function Home() {
                         <img src="src/assets/profile.webp" alt="picture of the website developer" />
                     </div>
                     <div className='hero-text'>
-                        Blurb
+                        <h2>Welcome!</h2>
+                        <p>
+                            I am David Shaw a passionate frontend developer with three years of hands-on experience and a degree in Information Systems from the University of Utah. Currently based at the University of Tennessee creating high impact web applications for the Libraries. Always eager to expand my knowledge and skills by embracing new challenges and opportunities for growth.
+                        </p>
                     </div>
                 </div>
             </div>
