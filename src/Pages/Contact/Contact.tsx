18fa1ae7e76816f0c57d0d43a6db1bcc26ff62d9
David Shaw <david.shaw1242@gmail.com>
2025-02-26 20:07:49 -0500

    contact layout setup

--- src/Pages/Contact/Contact.tsx
+++ src/Pages/Contact/Contact.tsx
@@ -12,10 +12,8 @@ export default function Contact() {
                     <h2 className="contact-header">How to Contact Me</h2>
                     <h3>Feel free to reach out to me with any inquiries at the following:</h3>
                     <div className='contact-info'>
-                        <p>Check Out My Resume: 
-                            <a href="https://docs.google.com/document/d/1L51oQwJK6h2nFfVH4JbomAKZ7ezcoW7V/edit?usp=sharing&ouid=101345781807643657466&rtpof=true&sd=true"> Here</a>
-                        </p>
-                        <p>Email me: <a href="mailto:david.shaw1242@gmail.com">Here</a></p>
+                        <p>Check Out My Resume: <a href="https://docs.google.com/document/d/1L51oQwJK6h2nFfVH4JbomAKZ7ezcoW7V/edit?usp=sharing&ouid=101345781807643657466&rtpof=true&sd=true">Here</a></p>
+                        <p>Email me: <a href="mailto:david.shaw1242@gmail.com">david.shaw1242@gmail.com</a></p>
                     </div>
                 </div>
             </div>
