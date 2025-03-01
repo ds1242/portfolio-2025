9e75bb2c1006914acd7d20e13c5a43addcc56147
David Shaw <david.shaw1242@gmail.com>
2025-03-01 15:48:16 -0500

    added some conditional rendering to the back card

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,4 +1,5 @@
 import "./ProjectCard.css";
+import { useState } from "react";
 interface ProjectInfoProps {
     ProjectInfo: {
         title: string;
@@ -11,18 +12,28 @@ interface ProjectInfoProps {
 };
 
 export default function ProjectCard({ ProjectInfo }: ProjectInfoProps) {
+
+    
+
     return (
         <>
-            <div className="flip-card">
+            <div className="flip-card link-underline">
                 <div className="flip-card-inner">
                     <div className="flip-card-front">
                         <img src={ProjectInfo.image} alt="" />
                     </div>
                     <div className="flip-card-back">
                         <h2>{ProjectInfo.title}</h2>
-                        <a className="">See More Details</a>
-                        {ProjectInfo.github ? <a className="">See Github</a> : ' '}
-                        
+                        <div className="flip-card-back-inner">
+                            <div>
+                                <button className="">See More Details</button>
+                            </div>
+                            {ProjectInfo.github ?
+                                <div className="card-github">
+                                    <a className="" href={ProjectInfo.github}>See Github</a>
+                                </div>
+                                : ' '}
+                        </div>
                     </div>
                 </div>
             </div>
